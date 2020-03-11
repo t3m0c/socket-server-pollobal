75107dd413e8fa784e3ef638004ed642ac3e3f91
Cuauhtemoc Olivares Pizano <c_olivares_pizano@hotmail.com>
2020-03-11 13:21:47 -0600

    Seccion 4

--- Classes/server.ts
+++ Classes/server.ts
@@ -1,16 +1,47 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+import socketIO from 'socket.io';
+import http from 'http';
+import * as socket from '../sockets/socket'
 
 export default class Server {
-    public app: express.Application;
-    public port: number;
+  private static _instance: Server;
 
-    constructor(){
-        this.app = express();
-        this.port = SERVER_PORT;
-    }
+  public app: express.Application;
+  public port: number;
 
-    start( callback: Function ) {
-        this.app.listen( this.port, callback() );
-    }
+  public io: socketIO.Server;
+  private httpServer: http.Server;
+
+  private constructor() {
+    this.app = express();
+    this.port = SERVER_PORT;
+
+    this.httpServer = new http.Server(this.app);
+    this.io = socketIO(this.httpServer);
+
+    this.escucharSockets();
+  }
+
+  public static get instance() {
+    return this._instance || (this._instance = new this());
+  }
+
+  private escucharSockets() {
+    console.log('Escuchando conexiones - sockets');
+
+    this.io.on('connection', cliente => {
+      console.log('Cliente conectado');
+
+      // Mensajes
+      socket.mensaje(cliente, this.io);
+
+      // desconectar
+      socket.desconectar(cliente);
+    });
+  }
+
+  start(callback: Function) {
+    this.httpServer.listen(this.port, callback());
+  }
 }
